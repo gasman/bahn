9f3e00191728cf4ded69657235f012bb75860f5f
Matt Westcott <matt@west.co.tt>
2009-05-16 01:08:43 +0100

    Eliminate Departure as a separate concept from Stop

--- bahn.rb
+++ bahn.rb
@@ -13,71 +13,21 @@ module Bahn
 		response_json['suggestions']
 	end
 	
-	class ClockTime
-		include Comparable
-
-		# represents an offset past midnight on a non-specific date, counted in days, hours and minutes
-		def initialize(days = 0, hours = 0, minutes = 0)
-			@days = days
-			@hours = hours
-			@minutes = minutes
+	class ClockTime < Time
+		# represents a time without a date
+		def self.clock(hours, mins)
+			self.utc(1970, 1, 1, hours, mins, 0)
 		end
 		
-		attr_reader :days, :hours, :minutes
+		def self.parse(str)
+			if str =~ /(\d+):(\d+)/ then self.utc(1970, 1, 1, $1, $2, 0) end
+		end
 		
+		def inspect
+			"#{hour}:#{min}"
+		end
 		def to_s
-			out = sprintf("%02d:%02d", @hours, @minutes)
-			if @days == 1
-				out << " +1 day"
-			elsif @days > 1
-				out << " +#{@days} days"
-			end
-			out
-		end
-		
-		def <=>(other)
-			[@days, @hours, @minutes] <=> [other.days, other.hours, other.minutes]
-		end
-		
-		def to_seconds
-			((@days * 24 + @hours) * 60 + @minutes) * 60
-		end
-		
-		def +(seconds)
-			ClockTime.seconds(self.to_seconds + seconds)
-		end
-
-		def -(other_time)
-			self.to_seconds - other_time.to_seconds
-		end
-		
-		def self.seconds(s)
-			minutes = s / 60
-			hours = minutes / 60
-			days = hours / 24
-			self.new(days, hours % 24, minutes % 24)
-		end
-		
-		def self.parse(str, opts = {})
-			case str
-				when /(\d+):(\d+) \+(\d+) day/
-					return self.new($3.to_i, $1.to_i, $2, to_i)
-				when /(\d+):(\d+)/
-					hours = $1.to_i
-					mins = $2.to_i
-					if opts[:after]
-						if ([hours, mins] <=> [opts[:after].hours, opts[:after].minutes]) == -1
-							# new date appears to be 'before' the after date, so add 1 day
-							return self.new(opts[:after].days + 1, hours, mins)
-						else
-							return self.new(opts[:after].days, hours, mins)
-						end
-					else
-						return self.new(0, hours, mins)
-					end
-				else
-					return nil
-			end
+			"#{hour}:#{min}"
 		end
 	end
 	
@@ -124,7 +74,7 @@ module Bahn
 		end
 		
 		def departures
-			DepartureBoard.new("http://reiseauskunft.bahn.de/bin/bhftafel.exe/en?input=#{@id}&productsFilter=1111100000&start=1&boardType=dep&dateBegin=14.12.08&dateEnd=12.12.09&time=00:00")
+			DepartureBoard.new(self)
 		end
 		
 		# =====
@@ -144,8 +94,9 @@ module Bahn
 	
 	class DepartureBoard
 		include Enumerable
-		def initialize(url_prefix)
-			@url_prefix = url_prefix
+		def initialize(station)
+			@station = station
+			@url_prefix = "http://reiseauskunft.bahn.de/bin/bhftafel.exe/en?input=#{station.id}&productsFilter=1111100000&start=1&boardType=dep&dateBegin=14.12.08&dateEnd=12.12.09&time=00:00"
 			@departure_pages = []
 		end
 		
@@ -158,22 +109,44 @@ module Bahn
 				departure_docs.each do |departure_doc|
 					service_link = departure_doc % 'td.train:nth-child(1) a'
 					destination_link = departure_doc % 'td.route span.bold a'
-					(destination_id, arrival_time), = destination_link['onclick'].scan(/^sHC\(this, '', '(\d+)','([^']*)'\)/)
+					(destination_id, destination_time_string), = destination_link['onclick'].scan(/^sHC\(this, '', '(\d+)','([^']*)'\)/)
+					destination_time = ClockTime.parse(destination_time_string)
+
 					intermediate_stops =  (departure_doc % 'td.route').children.find_all{|node| node.text?}.join(' ')
 					departure_time = ClockTime.parse((departure_doc % 'td.time').inner_text)
 					last_time = departure_time
-					intermediate_stops.scan(/\d+:\d+/) do |time|
-						last_time = ClockTime.parse(time, :after => last_time)
-					end
-					arrival_time = ClockTime.parse(arrival_time, :after => last_time)
+					days_passed = 0
 
-					yield Departure.new(
-						:time => departure_time,
-						:service_name => service_link.inner_text.strip.gsub(/\s+/, ' '),
-						:service_path => service_link['href'].sub(/\?.*/, ''),
-						:destination_name => destination_link.inner_text.strip,
-						:destination_id => destination_id,
-						:arrival_time => arrival_time
+					intermediate_stops.scan(/\d+:\d+/) do |time_string|
+						time = ClockTime.parse(time_string)
+						days_passed += 1 if time < last_time
+						last_time = time
+					end
+					inferred_time_to_destination = days_passed * 24*60*60 + (destination_time - departure_time)
+					
+					platform_td = departure_doc % 'td.platform'
+					if platform_td.nil?
+						platform = :none
+					else
+						platform = platform_td.inner_text.strip
+					end
+
+					yield Stop.new(
+						:station => @station,
+						:service => Service.new(
+							:path => service_link['href'].sub(/\?.*/, ''),
+							:name => service_link.inner_text.strip.gsub(/\s+/, ' '),
+							:destination_info => {
+								:station => Station.new(
+									:id => destination_id,
+									:name => destination_link.inner_text.strip
+								),
+								:arrival_time => destination_time
+							}
+						),
+						:departure_time => departure_time,
+						:platform => (platform == '' ? :none : platform),
+						:inferred_time_to_destination => inferred_time_to_destination
 					)
 				end
 			end
@@ -187,81 +160,86 @@ module Bahn
 		end
 	end
 	
-	class Departure
-		def initialize(opts)
-			@time = opts[:time]
-			@service_name = opts[:service_name]
-			@service_path = opts[:service_path]
-			@destination_name = opts[:destination_name]
-			@destination_id = opts[:destination_id]
-			@arrival_time = opts[:arrival_time]
-		end
-
-		attr_reader :time, :service_name, :destination_name, :destination_id, :arrival_time
-		
-		def destination
-			@destination ||= Station.new(:id => @destination_id, :name => @destination_name)
-		end
-		
-		def service
-			@service ||= Service.new(
-				:path => @service_path,
-				:name => @service_name,
-				:destination => destination,
-				:arrival_time => @arrival_time
-			)
-		end
-		
-		def to_s
-			"#{service_name} #{@time} #{@destination_name}"
-		end
-	end
-
 	class Service
 		def initialize(opts)
 			@path = opts[:path]
 			@name = opts[:name]
-			@destination = opts[:destination]
-			@arrival_time = opts[:arrival_time]
+			if opts[:origin_info]
+				@origin = Stop.new(opts[:origin_info].merge(
+					:service => self,
+					:arrival_time => :none,
+					:arrival_time_from_origin => :none,
+					:arrival_time_to_destination => :none,
+					:departure_time_from_origin => 0
+				))
+			end
+			if opts[:destination_info]
+				@destination = Stop.new(opts[:destination_info].merge(
+					:service => self,
+					:arrival_time_to_destination => 0,
+					:departure_time => :none,
+					:departure_time_from_origin => :none,
+					:departure_time_to_destination => :none
+				))
+			end
 		end
 		
 		def stops
-			stop_docs = traininfo_response_doc / "table.result tr[td.station]"
-			last_time = nil
-			stop_docs.collect {|stop_doc|
-				station_link = stop_doc % 'td.station a'
+			if !@stops
+				stop_docs = traininfo_response_doc / "table.result tr[td.station]"
+				last_time = nil
+				days_passed = 0
+				
+				origin_departure_time = nil
+				
+				@stops = stop_docs.collect {|stop_doc|
+					station_link = stop_doc % 'td.station a'
+					
+					arrival_time = ClockTime.parse((stop_doc % 'td.arrival').inner_text)
+					departure_time = ClockTime.parse((stop_doc % 'td.departure').inner_text)
+					origin_departure_time ||= departure_time
+					
+					if arrival_time.nil?
+						arrival_time_from_origin = nil
+					else
+						days_passed += 1 if (!last_time.nil? && arrival_time < last_time)
+						arrival_time_from_origin = days_passed + 24*60*60 + (arrival_time - origin_departure_time)
+						last_time = arrival_time
+					end
+					
+					if departure_time.nil?
+						departure_time_from_origin = nil
+					else
+						days_passed += 1 if (!last_time.nil? && departure_time < last_time)
+						departure_time_from_origin = days_passed + 24*60*60 + (departure_time - origin_departure_time)
+						last_time = departure_time
+					end
+	
+					platform = (stop_doc % 'td.platform').inner_text.strip
 
-				arrival_time = ClockTime.parse((stop_doc % 'td.arrival').inner_text, :after => last_time)
-				last_time = arrival_time unless arrival_time.nil?
-
-				departure_time = ClockTime.parse((stop_doc % 'td.departure').inner_text, :after => last_time)
-				last_time = departure_time unless departure_time.nil?
-
-				platform = (stop_doc % 'td.platform').inner_text.strip
-				Stop.new(
-					:station_name => station_link.inner_text,
-					:station_id => station_link['href'].scan(/&input=[^&]*%23(\d+)&/).first.first,
-					:arrival_time => arrival_time,
-					:departure_time => departure_time,
-					:platform => (platform == '' ? nil : platform)
-				)
-			}
+					Stop.new(
+						:station => Station.new(
+							:id => station_link['href'].scan(/&input=[^&]*%23(\d+)&/).first.first,
+							:name => station_link.inner_text
+						),
+						:service => self,
+						:arrival_time => arrival_time || :none,
+						:departure_time => departure_time || :none,
+						:platform => (platform == '' ? :none : platform),
+						:arrival_time_from_origin => arrival_time_from_origin,
+						:departure_time_from_origin => departure_time_from_origin
+					)
+				}
+			end
+			@stops
 		end
 		
-		class Stop
-			def initialize(opts)
-				@station_name = opts[:station_name]
-				@station_id = opts[:station_id]
-				@arrival_time = opts[:arrival_time]
-				@departure_time = opts[:departure_time]
-				@platform = opts[:platform]
-			end
+		def origin
+			@origin ||= stops.first
+		end
 
-			attr_reader :station_name, :station_id, :arrival_time, :departure_time, :platform
-			
-			def station
-				Station.new(:id => @station_id, :name => @station_name)
-			end
+		def destination
+			@destination ||= stops.last
 		end
 		
 		# =====
@@ -269,6 +247,101 @@ module Bahn
 		# =====
 		def traininfo_response_doc
 			@traininfo_response_doc ||= Hpricot(open("http://reiseauskunft.bahn.de#{@path}"))
+		end
+	end
+	
+	class Stop
+		def initialize(opts)
+			# for the following fields, use :none to mean none supplied (as opposed to not fetched yet):
+			# @arrival_time, @departure_time, @platform, @arrival_time_from_origin, @departure_time_from_origin
+
+			@station = opts[:station] # required
+			@service = opts[:service] # required
+			@arrival_time = opts[:arrival_time]
+			@departure_time = opts[:departure_time] # arrival_time or departure_time is required
+			@platform = opts[:platform]
+			@arrival_time_from_origin = opts[:arrival_time_from_origin]
+			@departure_time_from_origin = opts[:departure_time_from_origin]
+
+			@inferred_time_to_destination = opts[:inferred_time_to_destination]
+			
+			# these can be calculated from arrival_time_from_origin and departure_time_from_origin
+			# (but require service.origin, and therefore service.stops, to have been looked up)
+			@arrival_time_to_destination = opts[:arrival_time_to_destination]
+			@departure_time_to_destination = opts[:departure_time_to_destination]
+		end
+		
+		attr_reader :station, :service
+		
+		def platform
+			get_full_details if @platform.nil?
+			@platform == :none ? nil : @platform
+		end
+		
+		def arrival_time
+			get_full_details if @arrival_time.nil?
+			@arrival_time == :none ? nil : @arrival_time
+		end
+
+		def departure_time
+			get_full_details if @departure_time.nil?
+			@departure_time == :none ? nil : @departure_time
+		end
+		
+		def arrival_time_from_origin
+			get_full_details if @arrival_time_from_origin.nil?
+			@arrival_time_from_origin == :none ? nil : @arrival_time_from_origin
+		end
+		
+		def departure_time_from_origin
+			get_full_details if !@departure_time_from_origin.nil?
+			@departure_time_from_origin == :none ? nil : @departure_time_from_origin
+		end
+		
+		def arrival_time_to_destination
+			if @arrival_time_to_destination.nil?
+				if arrival_time_from_origin == :none
+					@arrival_time_to_destination == :none
+				else
+					@arrival_time_to_destination = @service.destination.arrival_time_from_origin - arrival_time_from_origin
+				end
+			end
+			@arrival_time_to_destination
+		end
+
+		def departure_time_to_destination
+			if @departure_time_to_destination.nil?
+				if departure_time_from_origin == :none
+					@departure_time_to_destination == :none
+				else
+					@departure_time_to_destination = @service.destination.arrival_time_from_origin - departure_time_from_origin
+				end
+			end
+			@departure_time_to_destination
+		end
+		
+		def inferred_time_to_destination
+			@inferred_time_to_destination ||= departure_time_to_destination || arrival_time_to_destination
+		end
+
+		# =====
+		private
+		# =====
+		def get_full_details
+			if @arrival_time and @arrival_time != :none
+				stop = @service.stops.find {|stop|
+					stop.station.id = @station.id && stop.arrival_time == @arrival_time
+				}
+				@departure_time = stop.departure_time || :none
+			else
+				stop = @service.stops.find {|stop|
+					stop.station.id = @station.id && stop.departure_time == @departure_time
+				}
+				@arrival_time = stop.arrival_time || :none
+			end
+			@platform = stop.platform || :none
+			@arrival_time_from_origin = stop.arrival_time_from_origin || :none
+			@departure_time_from_origin = stop.departure_time_from_origin || :none
 		end
 	end
 end
